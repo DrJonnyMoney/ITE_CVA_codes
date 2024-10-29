f4c5ad4275406100e194eb419cd2331d2a646a95
DrJonnyMoney <jonny_huang@ite.edu.sg>
2024-10-29 10:28:52 +0800

    Update and rename servo.py to servo_test.py

--- servo_test.py
+++ servo_test.py
@@ -7,7 +7,7 @@ import time
 # minimum pulse width, and maximum pulse width
 
 servo = AngularServo(18, min_pulse_width=0.0006, max_pulse_width=0.0023)
-servo.angle = 80
+servo.angle = 0
 
 servo.angle = 80
 
